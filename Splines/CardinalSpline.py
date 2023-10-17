7ad76840395d7528f7aeafc441100abe55d9cbf2
Tyler Kranz <tyler.kranz@ibm.com>
2023-10-17 15:28:06 -0500

    Rewrite of CardinalSpline to fix errors

--- Splines/CardinalSpline.py
+++ Splines/CardinalSpline.py
@@ -51,67 +51,31 @@ class CardinalSpline():
     
     def setPoint(self, index, point):
         self.pointCloud[index] = point
+        self.velocities = self.generateVelocities(self.pointCloud)
+
         if index != 0 and index != len(self.pointCloud)-1:
             self.curves[index].pointCloud[0] = point
             self.curves[index-1].pointCloud[-1] = point
-            
+
+            currentCurvePoint = self.curves[index].pointCloud[0]
+            currentCurveEndPoint = self.curves[index].pointCloud[-1]
+            previousCurvePoint = self.curves[index-1].pointCloud[-1]
+            previousCurveStartPoint = self.curves[index-1].pointCloud[0]
+            self.curves[index].pointCloud[1] = [currentCurvePoint[0]+(self.velocities[index][0]/3), currentCurvePoint[1]+(self.velocities[index][1]/3)]
+            self.curves[index].pointCloud[2] = [currentCurveEndPoint[0]-(self.velocities[index+1][0]/3), currentCurveEndPoint[1]-(self.velocities[index+1][1]/3)]
+            self.curves[index-1].pointCloud[1] = [previousCurveStartPoint[0]+(self.velocities[index-1][0]/3),previousCurveStartPoint[1]+(self.velocities[index-1][1]/3)]
+            self.curves[index-1].pointCloud[2] = [previousCurvePoint[0]-(self.velocities[index][0]/3), previousCurvePoint[1]-(self.velocities[index][1]/3)]
+
         elif index == len(self.pointCloud)-1:
             self.curves[index-1].pointCloud[-1] = point
-            
+            previousCurvePoint = self.curves[index-1].pointCloud[-1]
+            previousCurveStartPoint = self.curves[index-1].pointCloud[0]
+            self.curves[index-1].pointCloud[1] = [previousCurveStartPoint[0]+(self.velocities[index-1][0]/3),previousCurveStartPoint[1]+(self.velocities[index-1][1]/3)]
+            self.curves[index-1].pointCloud[2] = [previousCurvePoint[0]-(self.velocities[index][0]/3), previousCurvePoint[1]-(self.velocities[index][1]/3)]
+
         else:
             self.curves[index].pointCloud[0] = point
-
-        if index == 0:
-            vx = (self.pointCloud[0][0] - self.pointCloud[1][0])
-            vy = (self.pointCloud[0][1] - self.pointCloud[1][1])
-            tmp = [self.pointCloud[0][0] + vx, self.pointCloud[0][1] + vy]
-            self.velocities[0] = [(self.pointCloud[1][0]-tmp[0])*self.tension, (self.pointCloud[1][1]-tmp[1])*self.tension]
-            
             currentCurvePoint = self.curves[0].pointCloud[0]
-            self.curves[0].pointCloud[1] = [currentCurvePoint[0]+(self.velocities[0][0]/3), currentCurvePoint[1]+(self.velocities[0][1]/3)]
-            
-        elif index == len(self.pointCloud) - 1:
-            vx = (self.pointCloud[index][0] - self.pointCloud[index-1][0])
-            vy = (self.pointCloud[index][1] - self.pointCloud[index-1][1])
-            tmp = [self.pointCloud[index][0] + vx, self.pointCloud[index][1] + vy]
-            self.velocities[index] = [(tmp[0]-self.pointCloud[index-1][0])*self.tension, (tmp[1]-self.pointCloud[index-1][1])*self.tension]
-
-            f = self.velocities[index]
-            previousCurvePoint = self.curves[index-1].pointCloud[-1]
-            self.curves[index-1].pointCloud[-2] = [previousCurvePoint[0]-(self.velocities[index][0]/3), previousCurvePoint[1]-(self.velocities[index][1]/3)]
-            
-        elif index == 1:
-            vx1 = (self.pointCloud[0][0] - self.pointCloud[index][0])
-            vy1 = (self.pointCloud[0][1] - self.pointCloud[index][1])
-            tmp1 = [self.pointCloud[0][0] + vx1, self.pointCloud[0][1] + vy1]
-            self.velocities[index-1] = [(self.pointCloud[index][0]-tmp1[0])*self.tension, (self.pointCloud[index][1]-tmp1[1])*self.tension]
-
-            vx2 = (self.pointCloud[index+1][0] - self.pointCloud[index][0])
-            vy2 = (self.pointCloud[index+1][1] - self.pointCloud[index][1])
-
-            if len(self.pointCloud) == 3:
-                tmp2 = [self.pointCloud[index+1][0] + vx2, self.pointCloud[index+1][1] + vy2]
-                self.velocities[index+1] = [(tmp[0]-self.pointCloud[index][0])*self.tension, (tmp[1]-self.pointCloud[index][1])*self.tension]
-            
-            else:
-                self.velocities[index+1] = [(self.pointCloud[index+2][0]-self.pointCloud[index][0])*self.tension, (self.pointCloud[index+2][1]-self.pointCloud[index][1])*self.tension]
-
-            currentCurvePoint = self.curves[index].pointCloud[0]
-            previousCurvePoint = self.curves[index-1].pointCloud[-1]
+            currentCurveEndPoint = self.curves[0].pointCloud[-1]
             self.curves[index].pointCloud[1] = [currentCurvePoint[0]+(self.velocities[index][0]/3), currentCurvePoint[1]+(self.velocities[index][1]/3)]
-            self.curves[index-1].pointCloud[-2] = [previousCurvePoint[0]-(self.velocities[index][0]/3), previousCurvePoint[1]-(self.velocities[index][1]/3)]
-            
-        elif index == len(self.pointCloud) - 2:
-            vx2 = (self.pointCloud[index+1][0] - self.pointCloud[index][0])
-            vy2 = (self.pointCloud[index+1][1] - self.pointCloud[index][1])
-            tmp2 = [self.pointCloud[index+1][0] + vx2, self.pointCloud[index+1][1] + vy2]
-            self.velocities[index+1] = [(tmp2[0]-self.pointCloud[index][0])*self.tension, (tmp2[1]-self.pointCloud[index][1])*self.tension]
-
-            self.velocities[index-1] = [(self.pointCloud[index][0]-self.pointCloud[index-2][0])*self.tension, (self.pointCloud[index][1]-self.pointCloud[index-2][1])*self.tension]
-
-            currentCurvePoint = self.curves[index].pointCloud[0]
-            previousCurvePoint = self.curves[index-1].pointCloud[-1]
-            self.curves[index].pointCloud[1] = [currentCurvePoint[0]+(self.velocities[index][0]/3), currentCurvePoint[1]+(self.velocities[index][1]/3)]
-            self.curves[index-1].pointCloud[-2] = [previousCurvePoint[0]-(self.velocities[index][0]/3), previousCurvePoint[1]-(self.velocities[index][1]/3)]
-            
-        
+            self.curves[index].pointCloud[2] = [currentCurveEndPoint[0]-(self.velocities[index+1][0]/3), currentCurveEndPoint[1]-(self.velocities[index+1][1]/3)]
